cfd28c63b7e1111dca95888a104fc7641c79bfb2
TPN <tpn@ainta.dev>
2024-04-20 21:30:57 +0530

    fix smashystream

--- src/providers/sources/smashystream/index.ts
+++ src/providers/sources/smashystream/index.ts
@@ -1,27 +1,91 @@
+import { load } from 'cheerio';
+import Base64 from 'crypto-js/enc-base64';
+import Utf8 from 'crypto-js/enc-utf8';
+
 import { flags } from '@/entrypoint/utils/targets';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
 import { smashyStreamOScraper } from '@/providers/embeds/smashystream/opstream';
 import { smashyStreamFScraper } from '@/providers/embeds/smashystream/video1';
 import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 
+async function fetchCaptchaToken(ctx: MovieScrapeContext | ShowScrapeContext, domain: string, recaptchaKey: string) {
+  // from streamsb scraper
+  // by mrjvs
+  const domainHash = Base64.stringify(Utf8.parse(domain)).replace(/=/g, '.');
+
+  const recaptchaRender = await ctx.proxiedFetcher<string>(`https://www.google.com/recaptcha/api.js`, {
+    query: {
+      render: recaptchaKey,
+    },
+  });
+
+  const vToken = recaptchaRender.substring(
+    recaptchaRender.indexOf('/releases/') + 10,
+    recaptchaRender.indexOf('/recaptcha__en.js'),
+  );
+
+  const recaptchaAnchor = await ctx.proxiedFetcher<string>(
+    `https://www.google.com/recaptcha/api2/anchor?cb=1&hl=en&size=invisible&cb=flicklax`,
+    {
+      query: {
+        k: recaptchaKey,
+        co: domainHash,
+        v: vToken,
+      },
+    },
+  );
+
+  const cToken = load(recaptchaAnchor)('#recaptcha-token').attr('value');
+  if (!cToken) throw new Error('Unable to find cToken');
+
+  const tokenData = await ctx.proxiedFetcher<string>(`https://www.google.com/recaptcha/api2/reload`, {
+    query: {
+      v: vToken,
+      reason: 'q',
+      k: recaptchaKey,
+      c: cToken,
+      sa: '',
+      co: domain,
+    },
+    headers: { referer: 'https://www.google.com/recaptcha/api2/' },
+    method: 'POST',
+  });
+
+  const token = tokenData.match('rresp","(.+?)"');
+  return token ? token[1] : null;
+}
+
+const baseUrl = 'https://embed.smashystream.com';
+
 const universalScraper = async (ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> => {
-  // theres no point in fetching the player page
-  // because it too just calls the api with the tmdb id
-  // thats the only way to find out if the embed has any streams
+  const captchaPage = load(await ctx.proxiedFetcher('/videocaptcha.php', { baseUrl }));
+  const siteKey = captchaPage('.g-recaptcha').attr('data-sitekey');
+  if (!siteKey) throw new Error('Failed to get site key');
+
+  const token = await fetchCaptchaToken(ctx, baseUrl, siteKey);
+  if (!token) throw new Error('Failed to bypass recaptcha');
+
+  // this whitelists the ip
+  await ctx.proxiedFetcher('/getplayer.php', {
+    baseUrl,
+    method: 'POST',
+    body: new URLSearchParams({ 'g-recaptcha-response': token }),
+  });
+
   const query =
     ctx.media.type === 'movie'
       ? `?tmdb=${ctx.media.tmdbId}`
-      : `?tmdbId=${ctx.media.tmdbId}&season=${ctx.media.season.number}&episode=${ctx.media.episode.number}`;
+      : `?tmdb=${ctx.media.tmdbId}&season=${ctx.media.season.number}&episode=${ctx.media.episode.number}`;
 
   return {
     embeds: [
       {
         embedId: smashyStreamFScraper.id,
-        url: `https://embed.smashystream.com/video1dn.php${query}`,
+        url: `${baseUrl}/getplayer.php${query}&player=f`,
       },
       {
         embedId: smashyStreamOScraper.id,
-        url: `https://embed.smashystream.com/videoop.php${query}`,
+        url: `${baseUrl}/getplayer.php${query}&player=o`,
       },
     ],
   };
